f53c4b28653fcd0fce8049e7aea43c8c1e6bada9
albertgreinoecker <albert.greinoecker@gmail.com>
2019-05-22 12:53:40 +0200

    Konstruktor zur einfacheren Verwendung erzeugt Datentypen auf int gestellt zwecks einfacherer Verwendung

--- schach3b/src/org/htlanich/schach/Position.java
+++ schach3b/src/org/htlanich/schach/Position.java
@@ -2,8 +2,13 @@ package org.htlanich.schach;
 
 public class Position 
 {
-	public byte x;
-	public byte y;
+	public int x;
+	public int y;
 		
+	public Position(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
 	
 }
