29ff9e59a67b0a9a46f10ff3a8693fe8efad454d
aleks <aleks@10.10.217.60>
2019-05-22 14:30:57 +0200

    Kronstruktor mit default bewegtes false

--- schach3b/src/org/htlanich/schach/Turm.java
+++ schach3b/src/org/htlanich/schach/Turm.java
@@ -1,6 +1,12 @@
 package org.htlanich.schach;
 
 public class Turm extends Figur {
+	
+	public Turm(boolean farbeWeiss)
+	{
+		super(farbeWeiss, false);
+		
+	}
 
 	public Turm(boolean farbeWeiss, boolean bewegt) {
 		super(farbeWeiss, bewegt);
@@ -15,5 +21,11 @@ public class Turm extends Figur {
 	public boolean spielzugMoeglich(SpielFeld sp, Position von, Position nach) {
 		// TODO implement
 		return false;
+	}
+	@Override
+	public String toString()
+	{
+		
+		return "T" + super.toString();
 	}
 }
