628a498cd20f97b2128f7c9c148391224446ee04
HolziHW <daholzhammer@tsn.at>
2019-05-22 14:32:28 +0200

    Das Feld ist jetzt mit Buttons implementiert.

--- schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
+++ schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
@@ -2,16 +2,42 @@ package org.htlanich.schach;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class SpielfeldGUIfx extends Application implements EventHandler <MouseEvent>{
-
+	Button feld;
 	@Override
 	public void start(Stage primaryStage) {
-		
-	}
+        GridPane Spielfeld = new GridPane();
+        
+        for(int x = 0; x < 9;x++)
+        {
+        	for(int y = 0; y < 9; y++)
+        	{
+        		Button feld = new Button();
+        		feld.setPrefSize(60,60);
+        		feld.setMaxSize(60,60);
+        		Spielfeld.add(feld, x, y);
+        	}
+        }
+        
+        feld.setOnAction( event -> {
+        	feld.setBackground(Color.RED);
+        });
+        
+        Scene scene = new Scene(Spielfeld);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Spielfeld Schach");
+		primaryStage.show();
 
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -21,4 +47,6 @@ public class SpielfeldGUIfx extends Application implements EventHandler <MouseEv
 		// TODO Auto-generated method stub
 		
 	}
+	
+	
 }
