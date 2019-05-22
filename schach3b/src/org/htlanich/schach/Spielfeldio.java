78aebd669975ce5c4ac420cf1389ce859e440efc
albertgreinoecker <albert.greinoecker@gmail.com>
2019-05-22 12:35:47 +0200

    Klasse zum Einlesen von Spielfeldern aus einem Textfeld

--- schach3b/src/org/htlanich/schach/Spielfeldio.java
+++ schach3b/src/org/htlanich/schach/Spielfeldio.java
@@ -1,16 +1,65 @@
 package org.htlanich.schach;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Spielfeldio {
 	
-	public static SpielFeld einlesen(String fName)
+	/**
+	 * @param f z.B. BW oder __
+	 * @return ein Objekt vom Typ Feld, wenn es ein leeres Feld ist,
+	 * eine konkrete Figur, wenn es eine Spielfigur ist
+	 * Factory
+	 */	
+	private static Feld leseFeld(String f)
 	{
-		SpielFeld f = new SpielFeld();
-		//TODO implement
-		return f;
-	}
-
-	public static void main(String[] args) {
+		char typ = f.charAt(0); //z.B Bauer
+		char farbe = f.charAt(1); //z.B. W für Weiss
+		boolean farbeW = (farbe == 'W');
 		
+		switch(typ)
+		{
+			case 'T' : return new Turm(farbeW, false);
+			case 'S' : return new Springer(farbeW, false);
+			case 'L' : return new Laeufer(farbeW, false);
+			case 'K' : return new Koenig(farbeW, false);
+			case 'D' : return new Dame(farbeW, false);
+			case 'B' : return new Bauer(farbeW, false);
+			default: return new Feld();
+		}
+	}
+	
+	static public SpielFeld einlesen(String fName) throws FileNotFoundException
+	{
+		
+		Scanner s = new Scanner (new File(fName));
+		int zeile = 0;
+		Feld[][] feld = new Feld[8][8];
+		while (s.hasNextLine())
+		{
+			String line = s.nextLine(); //z.B. TW|SW|LW|DW|KW|LW|SW|TW
+			String[] figs = line.split("\\|");	//z.B. figs = {"TW", "SW" ,... }	
+			for (int spalte = 0; spalte < figs.length;spalte++)
+			{
+				Feld f = leseFeld(figs[spalte]); 
+				feld[zeile][spalte]  = f; //setze die aktuelle Figur auf das Spielfeld
+			}
+			zeile++;
+		}
+		s.close();
+		return new SpielFeld(feld);
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+			SpielFeld f = Spielfeldio.einlesen("start.txt");
+			f.ausgabe();
+			System.out.println(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
