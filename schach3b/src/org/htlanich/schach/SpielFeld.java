cec558a2096dc8bd3d0aadf5b989cbfdd9fef236
albertgreinoecker <albert.greinoecker@gmail.com>
2019-05-22 14:24:10 +0200

    ausgabe() implementiert

--- schach3b/src/org/htlanich/schach/SpielFeld.java
+++ schach3b/src/org/htlanich/schach/SpielFeld.java
@@ -37,7 +37,14 @@ public class SpielFeld {
 	}
 
 	public void ausgabe() {
-
+		for (int i = 0; i < mat.length;i++)
+		{
+			for (int j = 0; j < mat[0].length;j++)
+			{
+				System.out.print(mat[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 	public boolean schach() {
