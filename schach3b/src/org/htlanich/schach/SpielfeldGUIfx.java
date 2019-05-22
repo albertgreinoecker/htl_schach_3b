540aa97a3e0892a35c6e43fac9ca84b7e7b4ab46
HolziHW <daholzhammer@tsn.at>
2019-05-22 14:56:11 +0200

    Farbänderung beim drücken des Buttons

--- schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
+++ schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
@@ -5,17 +5,17 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class SpielfeldGUIfx extends Application implements EventHandler <MouseEvent>{
-	Button feld;
+	
 	@Override
 	public void start(Stage primaryStage) {
         GridPane Spielfeld = new GridPane();
-        
         for(int x = 0; x < 9;x++)
         {
         	for(int y = 0; y < 9; y++)
@@ -23,13 +23,19 @@ public class SpielfeldGUIfx extends Application implements EventHandler <MouseEv
         		Button feld = new Button();
         		feld.setPrefSize(60,60);
         		feld.setMaxSize(60,60);
+        		feld.setStyle("-fx-background-color: White");
+        		
+        		feld.setOnAction( event -> {
+        	
+        		feld.setStyle("-fx-background-color: Red");
+        		boolean pressed = true;
+        		
+        		});
         		Spielfeld.add(feld, x, y);
         	}
         }
         
-        feld.setOnAction( event -> {
-        	
-        });
+
         
         Scene scene = new Scene(Spielfeld);
 		primaryStage.setScene(scene);
