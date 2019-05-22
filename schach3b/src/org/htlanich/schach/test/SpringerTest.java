937019ddc1b9f6d3fa7495580b0b81f093bc9a0f
Fabi <Fabi@10.10.219.235>
2019-05-22 14:11:04 +0200

    2. Spielfeld zum Testen erzeugt + 4 Springer Tests

--- schach3b/src/org/htlanich/schach/test/SpringerTest.java
+++ schach3b/src/org/htlanich/schach/test/SpringerTest.java
@@ -12,10 +12,12 @@ import org.junit.jupiter.api.Test;
 
 class SpringerTest {
 	SpielFeld f1 = null;
+	SpielFeld f2 = null;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		f1 = Spielfeldio.einlesen("testfelder/S1.txt");
+		f2 = Spielfeldio.einlesen("testfelder/S2_waehrendSpiel.txt");
 	}
 
 	@AfterEach
@@ -25,14 +27,28 @@ class SpringerTest {
 	@Test
 	void testStartpositionNichtOk1() {
 		Springer s1 = (Springer)f1.getFeld(0, 1);
-		boolean ok1 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(0,2));
-		Assert.assertFalse(ok1);
+		boolean nichtOk1 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(0,2));
+		Assert.assertFalse(nichtOk1);
+	}
+	
+	@Test
+	void testStartpositionNichtOk2() {
+		Springer s1 = (Springer)f2.getFeld(4, 5);
+		boolean nichtOk2 = s1.spielzugMoeglich(f2, new Position(4,5), new Position(2,4));
+		Assert.assertFalse(nichtOk2);
 	}
 
 	@Test
-	void testStartpositionOk2() {
+	void testStartpositionOk1() {
 		Springer s1 = (Springer)f1.getFeld(0, 1);
-		boolean ok2 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(2,2));
+		boolean ok1 = s1.spielzugMoeglich(f1, new Position(0,1), new Position(2,2));
+		Assert.assertTrue(ok1);
+	}
+	
+	@Test
+	void testStartpositionOk2() {
+		Springer s1 = (Springer)f2.getFeld(4, 5);
+		boolean ok2 = s1.spielzugMoeglich(f2, new Position(4,5), new Position(2,6));
 		Assert.assertTrue(ok2);
 	}
 }
