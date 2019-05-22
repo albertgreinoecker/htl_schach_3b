cf0d965eb89925d989bbd1dbaedf2fbfde96b8f9
chris <chris@DESKTOP-8H8HRP3>
2019-05-22 14:36:42 +0200

    test ok fortgesetzt

--- schach3b/src/org/htlanich/schach/test/KoenigTest.java
+++ schach3b/src/org/htlanich/schach/test/KoenigTest.java
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.Test;
 
 class KoenigTest {
 	SpielFeld f1 = null;
+	SpielFeld f2 = null;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		f1 = Spielfeldio.einlesen("testfelder/S1.txt");
+		f2 = Spielfeldio.einlesen("testfelder/S2_waerhendSpiel");
 	}
 
 	@AfterEach
@@ -25,14 +27,41 @@ class KoenigTest {
 	}
 
 	@Test
-	void test() {
-		Koenig k1 = (Koenig)f1.getFeld(x, y);
-		boolean ok1 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(5,2));
+	void testOk1() {
+		Koenig k1 = (Koenig)f1.getFeld(5, 0);
+		boolean ok1 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(5,1));
 		Assert.assertTrue(ok1);
 	}
 	
 	@Test
-	void test() {
+	void testOk2() {
+		Koenig k1 = (Koenig)f1.getFeld(5, 0);
+		boolean ok2 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(4,0));
+		Assert.assertTrue(ok2);
+		
+	}
+	
+	@Test
+	void testOk3() {
+		Koenig k1 = (Koenig)f1.getFeld(5, 0);
+		boolean ok3 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(4,1));
+		Assert.assertTrue(ok3);
+		
+	}
+	
+	@Test
+	void testOk4() {
+		Koenig k1 = (Koenig)f1.getFeld(5, 0);
+		boolean ok4 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(6,1));
+		Assert.assertTrue(ok4);
+		
+	}
+	
+	@Test
+	void testOk5() {
+		Koenig k1 = (Koenig)f1.getFeld(5, 0);
+		boolean ok5 = k1.spielzugMoeglich(f1, new Position (5,0), new Position(6,0));
+		Assert.assertTrue(ok5);
 		
 	}
 
