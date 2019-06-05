e3ee0d41f06b3bc081d808e3a7f075211f070b2e
rapha <rapha@10.10.208.222>
2019-06-05 14:55:53 +0200

    erzeugt

---
+++ schach3b/src/org/htlanich/schach/LaeuferTest.java
@@ -0,0 +1,16 @@
+package org.htlanich.schach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LaeuferTest 
+{
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
