657f0073baeaf7f10c61d875f8472dcded04117f
HolziHW <daholzhammer@tsn.at>
2019-06-05 13:31:37 +0200

    Bilder für das Spielfeld hinzugefügt.

--- schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
+++ schach3b/src/org/htlanich/schach/SpielfeldGUIfx.java
@@ -1,9 +1,13 @@
 package org.htlanich.schach;
 
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
@@ -14,20 +18,26 @@ import javafx.stage.Stage;
 public class SpielfeldGUIfx extends Application implements EventHandler <MouseEvent>{
 	boolean pressed = false;
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		GridPane Spielfeld = new GridPane();
 		Spielfeld.getStylesheets().add(getClass().getResource("feldstyle.css").toExternalForm());
-        
+        SpielFeld sf = Spielfeldio.einlesen("start.txt");
 		
-		for(int x = 0; x < 9;x++)
+		for(int x = 0; x < 8;x++)
         {
-        	for(int y = 0; y < 9; y++)
+        	for(int y = 0; y < 8; y++)
         	{
+        		String datName = sf.getFeld(x, y) + ".png.png";
+        		Image fig = new Image(getClass().getResourceAsStream("Bilder/"+ datName));
         		Button feld = new Button();
+        		feld.setGraphic(new ImageView(fig));
         		feld.setPrefSize(60,60);
         		feld.setMaxSize(60,60);
         		feld.getStyleClass().add("standartFeld");
-        	       		
+        		        		
+        		
+        		
+        		
         		
         		feld.setOnAction( event -> {
         		if(pressed == true)
@@ -42,6 +52,7 @@ public class SpielfeldGUIfx extends Application implements EventHandler <MouseEv
                		pressed = true;
         		}
         		});
+        		
         		Spielfeld.add(feld, x, y);
         	}
         }
