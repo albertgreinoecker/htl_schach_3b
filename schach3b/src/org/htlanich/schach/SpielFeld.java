4f5e9c840b15cbb140eec00ba006a4a531d2acb6
albertgreinoecker <albert.greinoecker@gmail.com>
2019-06-07 09:15:20 +0200

    getKoenig und Beginn getFiguren

--- schach3b/src/org/htlanich/schach/SpielFeld.java
+++ schach3b/src/org/htlanich/schach/SpielFeld.java
@@ -48,12 +48,33 @@ public class SpielFeld {
 		}
 	}
 
+	private List<Position> getFiguren(boolean weiss)
+	{
+		
+	}
+	
+	private Position getKoenig(boolean weiss)
+	{
+		for (int i = 0; i < mat.length;i++)
+		{
+			for (int j = 0; j < mat[0].length;j++)
+			{
+				if (getFeld(i, j) instanceof Koenig)
+				{
+					Koenig k = (Koenig)getFeld(i, j);
+					if (k.isFarbeWeiss() == weiss)
+					{
+						return new Position(i, j);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
 	public boolean schach() {
 
-		/*if(Koenig.spielzugMoeglich = false )
-		{
-			return true;
-		} */
+		
 		return false;
 	}
 
