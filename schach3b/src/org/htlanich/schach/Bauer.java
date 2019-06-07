563b4747c5909429b4eaebdc07fde26328b51397
maxigreil <maxigreil@10.10.220.197>
2019-06-07 08:20:22 +0200

    spielzugmoeglich hinzugefügt

--- schach3b/src/org/htlanich/schach/Bauer.java
+++ schach3b/src/org/htlanich/schach/Bauer.java
@@ -1,5 +1,7 @@
 package org.htlanich.schach;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class Bauer extends Figur {
 	public Bauer(boolean farbeWeiss) {
 		super(farbeWeiss, false);
@@ -17,7 +19,22 @@ public class Bauer extends Figur {
 	}
 
 	public boolean spielzugMoeglich(SpielFeld sp, Position von, Position nach) {
-		// TODO: implement
+		int dx = Math.abs(von.x - nach.x);
+		int dy= Math.abs(von.y - nach.y);
+				
+		if( von.x == nach.x)
+		{
+		return(!super.isBewegt() && ((dy== 2)));
+		}else if( von.x == nach.x)
+		{
+		return(super.isBewegt() && (dy==1));
+		}else if( dx==1)
+		{
+			
+		}else
+		{
+			return false;
+		}
 		return false;
 	}
 	public String toString()
