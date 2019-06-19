c730e529ebb636ace647e83947378d4083edd07f
maxigreil <maxigreil@10.10.220.197>
2019-06-19 14:53:24 +0200

    Spielzug fertig

--- schach3b/src/org/htlanich/schach/Figur.java
+++ schach3b/src/org/htlanich/schach/Figur.java
@@ -26,24 +26,29 @@ public class Figur extends Feld {
 	}
 
 	public boolean spielZug(SpielFeld sp, Position von, Position nach) {
-		// TODO: implement
+		if (spielzugMoeglich(sp, von, nach)) {
+			Feld f = sp.getFeld(von.x, von.y);
+			sp.setFeld(nach.x, nach.y, f);
+			sp.setFeld(von.x, von.y, new Feld());
+			return true;
+		}
 		return false;
 	}
 
 	public boolean spielzugMoeglich(SpielFeld sp, Position von, Position nach) {
 		Feld nachFeld = sp.getFeld(nach.x, nach.x);
-		if (nachFeld instanceof Figur)
-		{
-			Figur f = (Figur)nachFeld;
+		if (nachFeld instanceof Figur) {
+			Figur f = (Figur) nachFeld;
 			return !von.equals(nach) && f.farbeWeiss != farbeWeiss;
 		}
 		return !von.equals(nach);
 	}
-	
+
 	@Override
 	public String toString() {
-		if (farbeWeiss) 
-			return "W"; 
-		else return "B";
+		if (farbeWeiss)
+			return "W";
+		else
+			return "B";
 	}
 }
