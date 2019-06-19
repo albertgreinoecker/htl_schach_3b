ea43eac57d76a0e4f833595fb851bbafd58de104
albertgreinoecker <albert.greinoecker@gmail.com>
2019-06-19 14:39:39 +0200

    Konstruktor mit default bewegt false

--- schach3b/src/org/htlanich/schach/Laeufer.java
+++ schach3b/src/org/htlanich/schach/Laeufer.java
@@ -6,9 +6,13 @@ public class Laeufer extends Figur
 	public Laeufer(boolean farbeWeiss, boolean bewegt) 
 	{
 		super(farbeWeiss, bewegt);
-		// TODO implement
 	}
 
+	public Laeufer(boolean farbeWeiss) 
+	{
+		super(farbeWeiss, false);
+	}
+	
 	public boolean spielZug(SpielFeld sp, Position von, Position nach) 
 	{
 		// TODO implement
